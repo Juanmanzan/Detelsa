e66f7677a2bc41b4d0ee954deedbbbacef04372f
Juanmanzan <140919209+Juanmanzan@users.noreply.github.com>
2025-05-16 09:34:05 -0500

    agregacion de boostrap y plantilla de inicio

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dootstra Laravel</title>
+     <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css')}}">
+</head>
+<body>
+    @yield('main_container')
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
