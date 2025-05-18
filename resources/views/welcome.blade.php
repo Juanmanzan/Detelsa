ae29ddae7b8079037f8a5ef816794cf408fc6bcb
gabyta02 <140862640+gabyta02@users.noreply.github.com>
2025-05-17 21:56:14 -0500

    Terminado del carusel en la pag de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,13 +9,15 @@
 
         <title>DETELSA</title>
         <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
 
     </head>
     <body>
         
-        <div class="Container">
+        <div class="container">
                      <!--Carusel-->
-            <div id="myCarousel" class="carousel slide mt-5 mb-6" data-bs-ride="carousel">
+            <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel">
                 <div class="carousel-indicators"> 
                     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button> 
                     <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button> 
@@ -23,12 +25,10 @@
                 </div> 
                 <div class="carousel-inner"> 
                     <div class="carousel-item active"> 
-                        <svg aria-hidden="true" class="bd-placeholder-img " height="100%" preserveAspectRatio="xMidYMid slice" width="100%" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
-                        </svg> 
+                        <img src="{{ asset('imagenes/producto1.jpg') }}" class="img-fluid mx-auto d-block carousel-img" alt="producto1">
                         <div class="container"> 
-                            <div class="carousel-caption text-start"> 
-                                <h1>Example headline.</h1> 
+                            <div class="carousel-caption text-start caption-bg"> 
+                                <h2>Example headline.</h2> 
                                 <p class="opacity-75">Some representative placeholder content for the first slide of the carousel.</p> 
                                 <p><a class="btn btn-lg btn-primary" href="#">Sign up today</a>
                                 </p> 
@@ -36,39 +36,40 @@
                         </div> 
                     </div> 
                     <div class="carousel-item"> 
-                        <svg aria-hidden="true" class="bd-placeholder-img " height="100%" preserveAspectRatio="xMidYMid slice" width="100%" xmlns="http://www.w3.org/2000/svg">
-                            <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
-                        </svg> 
+                        <img src="{{ asset('imagenes/producto1.jpg') }}" class="img-fluid mx-auto d-block carousel-img" alt="producto1">
                         <div class="container"> 
-                            <div class="carousel-caption"> 
+                            <div class="carousel-caption caption-bg"> 
                                 <h1>Another example headline.</h1> 
                                 <p>Some representative placeholder content for the second slide of the carousel.</p> 
                                 <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p> 
                             </div> 
                         </div> 
                     </div> 
-                <div class="carousel-item"> 
-                    <svg aria-hidden="true" class="bd-placeholder-img " height="100%" preserveAspectRatio="xMidYMid slice" width="100%" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect>
-                    </svg> 
-                    <div class="container"> 
-                        <div class="carousel-caption text-end"> 
-                            <h1>One more for good measure.</h1> 
-                            <p>Some representative placeholder content for the third slide of this carousel.</p> 
-                            <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p> 
+                    <div class="carousel-item"> 
+                        <img src="{{ asset('imagenes/producto1.jpg') }}" class="img-fluid mx-auto d-block carousel-img" alt="producto1"> 
+                        <div class="container"> 
+                            <div class="carousel-caption text-end caption-bg"> 
+                                <h1>One more for good measure.</h1> 
+                                <p>Some representative placeholder content for the third slide of this carousel.</p> 
+                                <p><a class="btn btn-lg btn-primary" href="#">Browse gallery</a></p> 
+                            </div> 
                         </div> 
                     </div> 
                 </div> 
-            </div> 
-            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev"> 
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span> 
-                <span class="visually-hidden">Previous</span> 
-            </button> 
-            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next"> 
-                <span class="carousel-control-next-icon" aria-hidden="true"></span> 
-                <span class="visually-hidden">Next</span> 
-            </button> 
-        </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev"> 
+                    <span class="custom-icon bg-success text-white rounded-circle p-2">
+                        <i class="bi bi-arrow-left"></i> 
+                    </span>
+                    <span class="visually-hidden">Previous</span> 
+                </button> 
+                    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next"> 
+                    <span class="custom-icon bg-success text-white rounded-circle p-2">
+                        <i class="bi bi-arrow-right"></i>
+                    </span>
+                    <span class="visually-hidden">Next</span> 
+                </button> 
+            </div>
+            <!--Categorias-->
         </div>
     </body>
 </html>
